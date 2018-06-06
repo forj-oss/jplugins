f379ef1d02d8e84d525839c78798454f3475f3e6
Christophe Larsonneur <clarsonneur@gmail.com>
2018-06-06 19:02:49 +0200

    list jenkins plugins version from an installation

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"os"
+
+	"github.com/alecthomas/kingpin"
+)
+
+// App is the application core struct
+var App jPluginsApp
+
 func main() {
-	
+	App.init()
+
+	switch kingpin.MustParse(App.app.Parse(os.Args[1:])) {
+	// Register user
+	case App.listInstalled.FullCommand():
+		App.doListInstalled()
+	}
 }
