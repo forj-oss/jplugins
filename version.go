6cd07023a1129d6d3d4a272e938c3c9e5224cfe5
Christophe Larsonneur <clarsonneur@gmail.com>
2018-06-08 18:58:40 +0200

    New 0.0.4

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	// VERSION application
-	VERSION = "0.0.3"
+	VERSION = "0.0.4"
 	// PRERELEASE = true if the version exposed is a pre-release
 	PRERELEASE = true
 	// AUTHOR is the project maintainer
