fdebd76291bb17507b0f3f54048684c7628705a0
Christophe Larsonneur <clarsonneur@gmail.com>
2018-06-14 16:20:35 +0200

    Prepare new version 0.0.7

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	// VERSION application
-	VERSION = "0.0.6"
+	VERSION = "0.0.7"
 	// PRERELEASE = true if the version exposed is a pre-release
 	PRERELEASE = true
 	// AUTHOR is the project maintainer
