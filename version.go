c98df4604dc1be9ea2372912c006be9cd57d12b2
Christophe Larsonneur <clarsonneur@gmail.com>
2018-11-21 14:46:17 +0100

    New release planned

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	// VERSION application
-	VERSION = "0.0.7"
+	VERSION = "0.0.8"
 	// PRERELEASE = true if the version exposed is a pre-release
 	PRERELEASE = true
 	// AUTHOR is the project maintainer
